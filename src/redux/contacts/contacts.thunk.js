2e50e25458281527da35a03fc3402c2468be2d4e
Serhii Manko <msa01051984@gmail.com>
2023-01-22 18:00:17 +0200

    thunk

--- src/redux/contacts/contacts.thunk.js
+++ src/redux/contacts/contacts.thunk.js
@@ -4,4 +4,14 @@ const contactsAPI = axios.create({
   baseURL: 'https://63cd4b0bd4d47898e39634b4.mockapi.io/contacts',
 });
 
-const fetchContacts = createAsyncThunk('users/fetchAll', async () => {});
+export const fetchContacts = createAsyncThunk(
+  'users/fetchAll',
+  async (_, thinkAPI) => {
+    try {
+      const { data } = await contactsAPI.get('/contacts');
+      return data;
+    } catch (error) {
+      return thinkAPI.rejectWithValue('error.message');
+    }
+  }
+);
